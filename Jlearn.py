20bc7e45f7f42c5f317b130745ccf6fc2848413b
lbattistelli <47535322+lbattistelli@users.noreply.github.com>
2019-03-01 12:06:47 +0100

    Add files via upload

---
+++ Jlearn.py
@@ -0,0 +1,160 @@
+import numpy as np
+import scipy.special  
+import seaborn as sns
+import matplotlib.pyplot as plt
+from yapf.yapflib.yapf_api import FormatCode
+
+class Jlearn:
+
+#__init__(self,n)
+#ambient(self,l)
+#computeV(self,currentposition)
+#strenght(self,currentposition,eta)
+#learn(self,path,eta,A)
+#zerostrenght(self)
+#hebbstrenght(self)
+#random_trajectory(self,t)
+#ordered_trajectory(self,t)
+#sample_regular_trajectory(self,epochs)
+#J_txt(A,l,eta,epochs)
+#grid_txt(B,l)
+
+    
+    def __init__(self,n):
+        self.n=n #linear dimension
+        
+        self.N=n*n #number of neurons
+        self.L=100 #lenght of the grid
+        self.sigma=1*np.float(self.L/n) 
+        self.c=1*np.float(self.L/n)*5 #cutoff
+        self.grid=np.zeros((pow(n,2),2)) #firing places' grid
+        self.J=np.zeros((n*n,n*n)) #connettivity matrix
+        self.hebbJ=np.zeros((n*n,n*n)) #hebbian CM 
+        self.V=np.zeros(n*n) #activity of the network
+    
+    
+    def ambient(self,l):#n=square root of number of neurons, l=randomness ,L dimension
+        for i in range(self.n):
+            for j in range(self.n):
+                self.grid[self.n*i+j][0]=np.random.uniform(np.float(self.L)/np.float(self.n)*(i),np.float(self.L)/np.float(self.n)*(i+l))
+                self.grid[self.n*i+j][1]=np.random.uniform(np.float(self.L)/np.float(self.n)*(j),np.float(self.L)/np.float(self.n)*(j+l))
+        return         
+    
+    def computeV(self,currentposition): #computes the activity of the network depending on the current position of the external input
+        for i in range(self.N):
+                self.V[i]=kernel(self.grid[i],currentposition,self.sigma,rat.c)
+    
+    def strenght(self,currentposition,eta): #upgrades CM with a learning rate eta dep. on the activity, fills J
+        deltaJ=np.zeros((self.N,self.N))
+        for i in range(self.N):
+            for j in range(self.N):
+                if j!=i:
+                    deltaJ[i][j]=eta*(self.V[i])*(self.V[j]) 
+                else:
+                    deltaJ[i][j]=0
+                    
+        for i in range(self.N):
+            for j in range(self.N):
+                self.J[i][j]=self.J[i][j]+deltaJ[i][j]
+        #a=sum(self.J)
+        #self.J=self.J/a
+        self.J=self.J/np.norm(self.J)
+            
+    def learn(self,path,eta,A): #total learning of the CM in t steps of a path
+        et=eta
+        t=path.shape[0]
+        m=np.zeros(t)
+        for s in range(t):
+            self.computeV(path[s]) #compute V on the current position of path's
+            self.strenght(path[s],et) #reinforcement of CM
+            m[s]=overlap(self.J,A) #evaluates overlap between J and A
+            if s%100==0:
+                print(s)
+        plot(np.arange(t),m) #plots overlap over t
+        xlabel('t')
+        ylabel('overlap')
+        show()
+        return m
+           
+            
+    
+    def zerostrenght(self): #generates a starting randm uniform CM
+        for i in range(self.N):
+            for j in range(self.N):
+                self.J[i][j]=np.random.randint(0,10,size=1)
+        #a=sum(self.J)
+        #self.J=self.J/a
+        self.J=self.J/np.norm(self.J)
+       
+        
+    def hebbstrenght(self): #generates an hebbian CM
+        for i in range(self.N):
+            for j in range(self.N):
+                self.hebbJ[i][j]=kernel(self.grid[i],self.grid[j],self.sigma,self.c)
+        #a=sum(self.hebbJ)
+        #self.hebbJ=self.hebbJ/a
+        self.hebbJ=self.hebbJ/np.norm(self.hebbJ)
+        
+
+    def random_trajectory(self,t):#x=storage position vector, t=time steps
+        x=np.zeros((t,2))
+        col=arange(t)
+        x[0][0]=np.random.uniform(0,self.L)
+        x[0][1]=np.random.uniform(0,self.L)
+        theta=np.zeros(t)
+        theta[0]=np.random.uniform(0,2*pi)
+        
+        for i in range(1,t):
+            x[i][0]=x[i-1][0]+0.5*(self.L/np.float(self.n))*np.cos(theta[i-1])
+            x[i][1]=x[i-1][1]+0.5*(self.L/np.float(self.n))*np.sin(theta[i-1])
+            theta[i]=np.random.normal(theta[i-1],pi/2,1)    
+            
+            if np.logical_or(x[i][0]>self.L,x[i][0]<0):
+                theta[i-1]=pi-theta[i-1]
+                x[i][0]=x[i-1][0]+0.5*(self.L/np.float(self.n))*np.cos(theta[i-1])
+            if np.logical_or(x[i][1]>self.L,x[i][1]<0):
+                theta[i-1]=-theta[i-1]
+                x[i][1]=x[i-1][1]+0.5*(self.L/np.float(self.n))*np.sin(theta[i-1])
+        #scatter(x.T[0],x.T[1],c=col,cmap=cm.jet)        
+        return x   
+            
+        #genera traiettoria random con hard bounds e inerzia direzonale
+        
+        
+    def ordered_trajectory(self,t):
+        x[0][0]=np.random.randint(0,self.N)
+        x[0][1]=np.random.randint(0,self.N)
+        while (x[i][0]==self.n and i<t):
+            x[i+1][0]=x[i][0]+np.random.choice([-1,0,1],size=1,p=[1/3,1/3,1/3])
+            x[i+1][1]=x[i][1]
+            
+    def sample_regular_trajectory(self,epochs): #genera traiettoria random. ciascun neurone viene visitato epochs volte. 
+        x=[]
+        pos=np.zeros((self.N,2))
+        for i in range(self.n):
+            for j in range(self.n):
+                pos[self.n*i+j][0]=i*(self.L/np.float(self.n))
+                pos[self.n*i+j][1]=j*(self.L/np.float(self.n))
+        for e in range(epochs):
+            np.random.shuffle(pos)
+            x.append(pos)
+        return np.asarray(x).reshape((self.N*epochs,2))
+
+    
+    def J_txt(A,l,eta,epochs): #prints J in a .txt reporting the trajectory,learning rate and epochs of the learning
+        B=np.matrix(A)
+        name_of_file="J("+np.str(l)+","+np.str(eta)+","+np.str(epochs)+")"
+        file=np.open(name_of_file + ".txt","w+")
+        for line in B:
+            np.savetxt(file,line,fmt='%.2f')
+        file.close()
+    
+    def grid_txt(B,l): #prints grid in a .txt reporting the l parameter for randomness
+        A=np.matrix(B)
+        name_of_file="grid("+np.str(np.len(B))+","+np.str(l)+")"
+        file=np.open(name_of_file + ".txt","w+")
+        for line in A:
+            np.savetxt(file,line,fmt='%.2f')
+        file.close()            
+
+print("ciao")
---
+++ functions.py
@@ -0,0 +1,169 @@
+import numpy as np
+import scipy.special  
+import seaborn as sns
+import matplotlib.pyplot as plt
+from yapf.yapflib.yapf_api import FormatCode
+
+class functions:
+#__init__(self,sig,c)
+#kernel(self,r_i,r_r)
+#computeV(mapping,currentposition)
+#overlap(matA,matB)
+#distance(r_1,r_2)
+#transfer(h,th,g)
+#positive_mean(v,th)
+#fix_parameters(V1,h1,th,a,a2)
+#dynamic(position,J,mapping)
+#random_trajectory(self,L,n,t)
+#ordered_trajectory(self,L,n,t)
+#sample_regular_trajectory(self,L,n,epochs)
+
+    
+    def __init__(self,sig,c):
+        self.sig=sig
+        self.c=c
+
+    def kernel(self,r_i,r_r):#r_i=position on the grid,r_r=rat position,sig=standard deviation,c=cutoff
+        V=0.0  
+        dx=0
+        dy=0
+        d=0
+        dx=np.abs(r_i[0]-r_r[0])
+        dy=np.abs(r_i[1]-r_r[1])
+        d=sqrt(pow(dx,2)+pow(dy,2))
+        if d<=self.c:
+            V=exp(-0.5*pow(d/self.sig,2))
+        else:
+            V=0    
+        return V
+
+    def computeV(mapping,currentposition): #computes the activity of the network depending on the current position of the external input
+        N=np.len(mapping)    
+        V=np.zeros(N)
+        for i in range(N):
+            V[i]=kernel(mapping[i],currentposition,self.sig,self.c)
+        return V
+
+    def overlap(matA,matB):
+        m=0
+        for i in range(matA.shape[0]):
+            for j in range(matA.shape[0]):
+                m=m+matA[i][j]*matB[i][j]
+        m=m/float(np.norm(matA)*np.norm(matB))
+        return m
+
+    def distance(r_1,r_2):
+        dx=0
+        dy=0
+        d=0
+        dx=np.abs(r_1[0]-r_2[0])
+        dy=np.abs(r_1[1]-r_2[1])
+        d=np.sqrt(pow(dx,2)+pow(dy,2))
+        return d
+
+#transfer function
+    def transfer(h,th,g):
+        if (h-th)>0:
+            return g*(h-th)
+        else:
+            return 0.0
+    
+    def positive_mean(v,th):
+        return np.mean([x-th for x in v if x-th>0])
+
+#interaction kernel
+
+    def fix_parameters(V1,h1,th,a,a2):
+        fixed=False
+        it=0
+        while (not fixed) and it<maxiter:
+            th=th+b*(pow(np.mean(V1),2)/np.mean(pow(V1,2))-a2)
+            V1=np.asarray(np.list(np.map(lambda h1: transfer(h1,th,g),h1)))
+            fixed=(np.abs((pow(np.mean(V1),2)/np.mean(pow(V1,2))-a2))/a2 <= sigma_mean)
+    #g=a/positive_mean(h1,th)
+        if it>=maxiter:
+            print("Iter bound reached")
+        return th
+
+    def dynamic(position,J,mapping): #evolves the activity with J starting from a bump in current position
+        #parameters
+        N=np.len(mapping[0])                           
+        xi=0.2                    
+        maxsteps=50
+        a=1.0 #mean activity
+        a2=0.1 #sparsity
+
+#for threshold fixing
+        b=0.01
+        sigma_mean=0.001
+        maxiter=10000000
+        lmbd=10
+#initialization
+        V=np.zeros(N)
+        h=np.zeros(N)
+        Vin=np.zeros(N)
+
+        V=self.computeV(position)
+        Vin=V
+    
+        g=1.1
+        th=0
+        for step in range(maxsteps):
+            h=np.dot(J,V)
+            V=np.asarray(np.list(np.map(lambda h: transfer(h,th,g),h)))
+            th=fix_parameters(V,h,th,a,a2)
+            V=np.asarray(np.list(np.map(lambda h: transfer(h,th,g),h)))
+            g=a/np.mean(V)
+            V=g*V
+        index=np.where(V==max(V))  
+        return mapping[index[0]][0]
+        #print("Dynamic step: "+str(step)+" done, mean: "+str(mean(V))+" sparsity: "+str(pow(mean(V),2)/mean(pow(V,2))))
+
+
+    def random_trajectory(self,L,n,t):#x=storage position vector, t=time steps
+        x=np.zeros((t,2))
+        col=arange(t)
+        x[0][0]=np.random.uniform(0,L)
+        x[0][1]=np.random.uniform(0,L)
+        theta=np.zeros(t)
+        theta[0]=np.random.uniform(0,2*pi)
+        
+        for i in range(1,t):
+            x[i][0]=x[i-1][0]+0.5*(L/np.float(n))*np.cos(theta[i-1])
+            x[i][1]=x[i-1][1]+0.5*(L/np.float(n))*np.sin(theta[i-1])
+            theta[i]=np.random.normal(theta[i-1],pi/2,1)    
+            
+            if np.logical_or(x[i][0]>L,x[i][0]<0):
+                theta[i-1]=pi-theta[i-1]
+                x[i][0]=x[i-1][0]+0.5*(L/np.float(n))*np.cos(theta[i-1])
+            if np.logical_or(x[i][1]>L,x[i][1]<0):
+                theta[i-1]=-theta[i-1]
+                x[i][1]=x[i-1][1]+0.5*(L/np.float(n))*np.sin(theta[i-1])
+        #scatter(x.T[0],x.T[1],c=col,cmap=cm.jet)        
+        return x   
+            
+        #genera traiettoria random con hard bounds e inerzia direzonale
+        
+        
+    def ordered_trajectory(self,L,n,t):
+        N=n*n
+        x[0][0]=np.random.randint(0,N)
+        x[0][1]=np.random.randint(0,N)
+        while (x[i][0]==n and i<t):
+            x[i+1][0]=x[i][0]+np.random.choice([-1,0,1],size=1,p=[1/3,1/3,1/3])
+            x[i+1][1]=x[i][1]
+            
+    def sample_regular_trajectory(self,L,n,epochs): #genera traiettoria random. ciascun neurone viene visitato epochs volte. 
+        x=[]
+        N=n*n
+        pos=np.zeros((N,2))
+        for i in range(n):
+            for j in range(n):
+                pos[n*i+j][0]=i*(L/np.float(n))
+                pos[n*i+j][1]=j*(L/np.float(n))
+        for e in range(epochs):
+            np.random.shuffle(pos)
+            x.append(pos)
+        return np.asarray(x).reshape((N*epochs,2))
+
+print("ciao")
